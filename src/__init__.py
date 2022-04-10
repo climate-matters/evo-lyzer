bad09ce4065eac18f1f320c4c439493f68a46504
Yasas <ysenarath.93@gmail.com>
2022-04-09 23:01:38 -0400

    Add authorship information

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Yasas Senarath'
+__copyright__ = 'Copyright 2021, The Evo-Lyzer Project'
+__credits__ = ['Yasas Senarath']
+__license__ = "MIT License"
+__version__ = '0.0.1'
+__maintainer__ = "Yasas Senarath"
+__email__ = 'ysenarath.93@gmail.com'
